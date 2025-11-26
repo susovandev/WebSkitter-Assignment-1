6f807d627d4a336be9c89e9cd33ae025d8b0ebb5
Susovan Dev <susovandas.dev@gmail.com>
2025-11-26 23:18:32 +0530

    feat(validation): add Joi schema for product creation and integrate Joi validation middleware

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const productController = require('../controller/product.controller');
-
+const validateSchema = require('../middlewares/validation.middleware');
+const productValidation = require('../validation/product.validation');
 const router = express.Router();
 
-router.route('/').post(productController.createProductHandler);
+router
+	.route('/')
+	.post(
+		validateSchema(productValidation.createProductSchema, 'body'),
+		productController.createProductHandler,
+	);
 
 module.exports = router;
